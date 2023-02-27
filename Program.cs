06b61f72b8442d253128416056ed028c5dc7cb8b
Tatsiana Malakhovskaya <butkovka@mail.ru>
2023-02-27 15:48:43 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 Console.Write("Input the first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the second number ");
@@ -20,3 +20,39 @@ else
         Console.WriteLine($"The max number is {number2}, the min number is {number1}");
     }
 }
+
+
+// Task 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+
+if(number1 > number2)
+{
+    if(number1 > number3)
+    {
+        Console.WriteLine("Max number is " + number1);
+    }
+    else
+    {
+       Console.WriteLine("Max number is " + number3); 
+    }
+}
+else
+{
+    if(number2 > number3)
+    {
+        Console.WriteLine("Max number is " + number2);
+    }
+    else
+    {
+     Console.WriteLine("Max number is " + number3);   
+    }
+}
+*/
+
