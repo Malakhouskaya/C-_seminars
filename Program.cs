41428c1b8b77143c94e747c930ec04f18daffd19
Tatsiana Malakhovskaya <butkovka@mail.ru>
2023-02-27 14:35:29 +0300

    Добавлены материалы с первого семинара + файл гитигнор

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*Task 1
+
+Console.Write("Input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = number2 * number2;
+
+if(number1 == quad2)
+    Console.WriteLine("yes!");
+else
+    Console.WriteLine("no!");
+*/
+
+
+/*Task 2 Напишите программу, кот. на вход принимает одно число (N), a на выходе показывает все целые числа в промежутке
+// от -N до N.
+
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;  // если переменную нужно изменить на какую-то переменную, пример, current +=2
+}
+
+
+34215/10 = 3421
+34215/100 = 342
+34215/1000 = 34
+34215/10000 = 3
+
+34215 % 10 = 5
+34215 % 100 = 15
+34215 % 1000 = 215
+34215 % 10000 = 4215
+
+23456 -> 34
+23456 / 100 % 100
+
+28563  -> 56
+28563 / 10 % 100
+
+*/
+
